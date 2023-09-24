9b3d98fcb3a85654ceea363c742cc5a5c2e78e7f
SajeedAninda <sajaninda@gmail.com>
2023-09-24 19:21:57 +0600

    Created Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
+        <div className='w-[90%] mx-auto'>
+        <Navbar></Navbar>
+        </div>
         <Outlet></Outlet>
     </>
   )
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from "../../../Resources/Logo.png";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div className='flex  justify-between items-center py-6'>
+            <div className="logo w-[180px]">
+                <img src={logo} alt="" />
+            </div>
+            <div className="links flex gap-10">
+                <NavLink
+                    to="/"
+                    className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "underline text-[#FF444A] font-bold" : ""
+                    }
+                >
+                    Home
+                </NavLink>
+
+                <NavLink
+                    to="/donation"
+                    className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "underline text-[#FF444A] font-bold" : ""
+                    }
+                >
+                    Donation
+                </NavLink>
+
+
+                <NavLink
+                    to="/stats"
+                    className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "underline text-[#FF444A] font-bold" : ""
+                    }
+                >
+                    Statistics
+                </NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
