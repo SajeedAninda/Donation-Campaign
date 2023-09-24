c79f3261f2de47d1c9d5cb65e38025c1e8a1966f
SajeedAninda <sajaninda@gmail.com>
2023-09-24 19:08:19 +0600

    Added Required Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Home from './Components/Home/Home.jsx'
+import Donation from './Components/Donation/Donation.jsx'
+import Statistics from './Components/Statistics/Statistics.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/donation",
+        element: <Donation></Donation>,
+      },
+      {
+        path: "/stats",
+        element: <Statistics></Statistics>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
